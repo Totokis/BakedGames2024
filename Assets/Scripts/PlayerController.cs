bcc215060c85be122b3fb6630eeff654932af75e
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2024-04-06 23:40:45 +0200

    Fixes sliding in walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -86,14 +86,6 @@ public class PlayerController : MonoBehaviour
         );
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        Collider myCollider = collision.GetContact(0).thisCollider;
-        // Now do whatever you need with myCollider.
-        // (If multiple colliders were involved in the collision, 
-        // you can find them all by iterating through the contacts)
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Wall"))
