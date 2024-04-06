fb2a253344da91b7dac6446f9651a7f3cb10e8fd
astisk <pobierac@gmail.com>
2024-04-06 19:57:28 +0200

    Score controller

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,30 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private Int32 _score = 0;
+    private TextMeshProUGUI _scoreText;
+    public Int32 Score => _score;
+    void Start()
+    {
+        _scoreText = GetComponent<TextMeshProUGUI>();
+        AddScore();
+    }
+    private void UpdateText()
+    {
+        _scoreText.text = $"{_score}";
+    }
+    public void AddScore()
+    {
+        _score += 1;
+        UpdateText();
+    }
+
+    public void ResetScore()
+    {
+        _score = 0;
+        UpdateText();
+    }
+}
