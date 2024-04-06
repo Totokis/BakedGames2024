354176741d38821e34a258be44533df8b029d79f
Michal Minikowski <michal.minikowski@q-perior.com>
2024-04-06 23:31:53 +0200

    asdasd

--- Assets/ShowersController.cs
+++ Assets/ShowersController.cs
@@ -4,6 +4,7 @@ public class ShowersController : MonoBehaviour
 {
     public static ShowersController Instance { get; private set; }
 
+    [SerializeField]
     private Shower[] showers;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
