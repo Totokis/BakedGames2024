bf6514a89b8cb185d1f606987691b59cafc0a4c0
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2024-04-07 13:43:57 +0200

    Makes player faster

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,8 @@ public class PlayerController : MonoBehaviour
     private Boolean canSlideLeft = true;
     private Boolean canSlideRight = true;
 
-    [SerializeField] private Single _speed = 250;
-    [SerializeField] private Single _slideSpeed = 400;
+    [SerializeField] private Single _speed = 285;
+    [SerializeField] private Single _slideSpeed = 415;
 
     private Rigidbody2D rb;
     private Collider2D collider;
