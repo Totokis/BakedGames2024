806863ebbd6c61f7fd21a48e831b002029010a2b
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2024-04-07 12:49:37 +0200

    Adds stamina bar

--- Assets/Scripts/StaminaController.cs
+++ Assets/Scripts/StaminaController.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaminaController : MonoBehaviour
 {
     public Int32 _staminaPoints;
 
     public Int32 SlideCost;
-    public Int32 RecoveryTime;
+    public Single RecoveryTime;
     public Int32 StaminaRecoveryValue;
     public Int32 StaminaPoints => _staminaPoints;
+
+    public Slider slider;
     void Start()
     {
         _staminaPoints = 100;
+        UpdateUI();
         StartCoroutine(AddStamina());
     }
 
@@ -23,22 +27,28 @@ public class StaminaController : MonoBehaviour
     public void UseSlide()
     {
         _staminaPoints -= 30;
-        if(_staminaPoints < 0)
+        UpdateUI();
+        if (_staminaPoints < 0)
+        {
             _staminaPoints = 0;
-    }
-
-    void Update()
-    {
-        print(_staminaPoints);
+            UpdateUI();
+        }
     }
 
     IEnumerator AddStamina()
     {
         if(_staminaPoints < 100)
+        {
             _staminaPoints += StaminaRecoveryValue;
+            UpdateUI();
+        }
 
         yield return new WaitForSeconds(RecoveryTime);
         StartCoroutine(AddStamina());
+    }
 
+    void UpdateUI()
+    {
+        slider.value = _staminaPoints;
     }
 }
