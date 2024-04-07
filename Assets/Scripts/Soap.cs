d805d7cb41cf4369e15e8314bfe481b08651725e
Michal Minikowski <michal.minikowski@q-perior.com>
2024-04-07 12:51:31 +0200

    asdasd

--- Assets/Scripts/Soap.cs
+++ Assets/Scripts/Soap.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class Soap : MonoBehaviour
 {
+    public AudioSource AudioSource;
+    public AudioClip SoapPickup;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<Portal>() != null)
@@ -17,12 +20,25 @@ public class Soap : MonoBehaviour
         if (collision.transform.CompareTag("Player") && !GameManager.Instance._isWarningDisplayed)
         {
             EventManager.Instance.AddScore();
-            Destroy(gameObject);
+            AudioSource.PlayOneShot(SoapPickup);
+            GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(GetComponent<Rigidbody2D>());
+
+            LeanTween.scale(gameObject, Vector3.zero, 0.5f)
+                .setEaseOutSine();
+
+
+            Invoke(nameof(GetRid), 2f);
         }
         else if (collision.transform.CompareTag("Floor"))
         {
             Invoke("DisplayWarning", 2f);
         }
+    }
+
+    private void GetRid()
+    {
+        Destroy(gameObject);
     }
 
     private void DisplayWarning()
