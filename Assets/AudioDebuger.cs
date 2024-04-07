65aa6d923ec1fe0ce09526d8dc256a4dd83454ee
Totokis <pawelj_janusz@wp.pl>
2024-04-07 15:03:21 +0200

    fix(Audio): Super hot fix audio OutroSequence.unity

--- Assets/AudioDebuger.cs
+++ Assets/AudioDebuger.cs
@@ -6,17 +6,38 @@ using UnityEngine.Audio;
 public class AudioDebuger : MonoBehaviour
 {
     [SerializeField]
-    private AudioClip testSound;
+    private AudioClip heartBit ;
+
+    [SerializeField]
+    private AudioClip endVoice;
+    
 
     [SerializeField]
     private AudioSource audioSource;
 
+    public void PlayHeartBit()
+    {
+        if(Application.platform == RuntimePlatform.WebGLPlayer) 
+            audioSource.PlayOneShot(heartBit);    
+        
+    }
+
+    public void StopHeartBit()
+    {
+        if(Application.platform == RuntimePlatform.WebGLPlayer)
+            audioSource.Stop();
+    }
     
+    public void PlayEndVoice()
+    {
+        if(Application.platform == RuntimePlatform.WebGLPlayer)
+            audioSource.PlayOneShot(endVoice);
+    }
 
-     private IEnumerator Start()
-     {
-         yield return new WaitForSeconds(3);
-
-         audioSource.PlayOneShot(testSound);
-     }
+    public void StopEndVoice()
+    {
+        if(Application.platform == RuntimePlatform.WebGLPlayer)
+            audioSource.Stop();
+    }
+    
 }
