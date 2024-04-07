c798a2d40d970054a09b9bc1b81ad85c92379332
Michal Minikowski <michal.minikowski@q-perior.com>
2024-04-07 04:04:22 +0200

    gryps

---
+++ Assets/GrypsController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+
+public class GrypsController : MonoBehaviour
+{
+    public AudioClip[] Grypses;
+
+    void Start()
+    {
+        var shuff = ArrayShuffler.GetShuffledIndices(Grypses.Length);
+        foreach(var shuffIndex in shuff) { print(shuffIndex); }
+    }
+
+    bool initial = true;
+    private IEnumerator StartGrypsing()
+    {
+        if(initial)
+            yield return new WaitForSeconds(6f);
+
+        initial = false;
+
+        yield return new WaitForSeconds(UnityEngine.Random.Range(8f, 12f));
+
+        StartCoroutine(StartGrypsing());
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+public class ArrayShuffler
+{
+    // Method to shuffle indices of an array using Unity's Random
+    public static int[] GetShuffledIndices(int arrayLength)
+    {
+        // Create an array of indices [0, 1, 2, ..., arrayLength-1]
+        int[] indices = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            indices[i] = i;
+        }
+
+        // Shuffle the indices array
+        Shuffle(indices);
+
+        return indices;
+    }
+
+    // Fisher-Yates shuffle algorithm adapted for Unity's Random
+    private static void Shuffle(int[] array)
+    {
+        int n = array.Length;
+        for (int i = n - 1; i > 0; i--)
+        {
+            // Pick a random index from 0 to i
+            int j = UnityEngine.Random.Range(0, i + 1);
+
+            // Swap array[i] with the element at random index
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
